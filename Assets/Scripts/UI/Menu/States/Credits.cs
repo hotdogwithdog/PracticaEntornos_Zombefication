dfbd8c44e84ccc91fc8841603f4ae69f93d329a8
hotdogwithdog <hotdogwithdog@gmail.com>
2025-05-13 20:30:06 +0200

    Added the input system well done left do the human zombie switch

---
+++ Assets/Scripts/UI/Menu/States/Credits.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UI.Menu.States
+{
+    internal class Credits : AMenuState
+    {
+        public Credits() : base("Menus/Credits") { }
+
+        protected override void OnOptionClicked(MenuButtons option)
+        {
+            switch (option)
+            {
+                case MenuButtons.Back:
+                    MenuManager.Instance.SetState(new Main());
+                    break;
+                default:
+                    Debug.LogError($"ERROR_UNKOWN_OPTION: {option}");
+                    return;
+            }
+        }
+        public override void Update(float deltaTime) { }
+    }
+}
